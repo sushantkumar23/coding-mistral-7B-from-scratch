78d372f8ca308817db9eb2edd1bfe38471e0573f
Sushant Kumar <sushantkumar23@gmail.com>
2023-12-18 16:59:15 +0530

    Added the code for KV cache

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 # model.py
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 
 import torch
 from torch import nn
+from xformers import AttentionBias
+from xformers.ops.fmha import memory_efficient_attention
 
 from dataclasses import dataclass
 from simple_parsing.helpers import Serializable
@@ -17,13 +19,82 @@ def apply_rotary_emb(xq: torch.Tensor, xk: torch.Tensor, freqs_cis: torch.Tensor
 
 
 
-class CacheView:
-    # TODO: Implement cacheview
+@dataclass
+class RotatingCacheInputMetadata:
+    # rope absolute positions
+    positions: torch.Tensor
+    # which elements in the sequences need to be cached
+    to_cache_mask: torch.Tensor
+    # how many elements are cached per sequence
+    cached_elements: torch.Tensor
+    # where tokens should go in the cache
+    cache_positions: torch.Tensor
 
-    def __init__(self, cache_k: torch.Tensor, cache_v: torch.Tensor):
+    # if prefill, use block diagonal causal mask
+    # else use causal with padded key mask
+    prefill: bool
+    mask: AttentionBias
+    seqlens: List[int]
+
+
+def interleave_list(l1: List[torch.Tensor], l2: List[torch.Tensor]):
+    assert len(l1) == len(l2)
+    return [v for pair in zip(l1, l2) for v in pair]
+
+
+def unrotate(cache: torch.Tensor, seqlen: int) -> torch.Tensor:
+    assert cache.ndim == 3 # (W, H, D)
+    position = seqlen % cache.shape[0]
+    if seqlen < cache.shape[0]:
+        return cache[:seqlen]
+    elif position == 0:
+        return cache
+    else:
+        return torch.cat([cache[position:], cache[:position]], dim=0)
+
+class CacheView:
+
+    def __init__(self, cache_k: torch.Tensor, cache_v: torch.Tensor, metadata: RotatingCacheInputMetadata, kv_seqlens: torch.Tensor):
         self.cache_k = cache_k
         self.cache_v = cache_v
+        self.kv_seqlens = kv_seqlens
+        self.metadata = metadata
 
+    def update(self, xk: torch.Tensor, xv: torch.Tensor):
+        """
+        to_cache_mask masks the last [sliding_window] tokens in each sequence
+        """
+        n_kv_heads, head_dim = self.cache_k.shape[-2:]
+        flat_cache_k = self.cache_k.view(-1, n_kv_heads, head_dim)
+        flat_cache_v = self.cache_v.view(-1, n_kv_heads, head_dim)
+
+        flat_cache_k.index_copy_(0, self.metadata.cache_positions, xk[self.metadata.to_cache_mask])
+        flat_cache_v.index_copy_(0, self.metadata.cache_positions, xv[self.metadata.to_cache_mask])
+
+    def interleave_kv(self, xk: torch.Tensor, xv: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Interleave the cached elements with the new elements
+        """
+        assert xk.ndim == xv.ndim == 3 # (seqlen, n_kv_heads, head_dim)
+        assert xk.shape == xv.shape
+
+        if all([s == 0 for s in self.metadata.seqlens]):
+            # No cache yet, just return the new elements
+            return xk, xv
+
+        # Make it a list of tensors
+        xk = torch.split(xk, self.metadata.seqlens)
+        xv = torch.split(xv, self.metadata.seqlens)
+        assert len(xk) == len(xv) == len(self.kv_seqlens), f"Batch size is {len(self.kv_seqlens)} but got {len(xk)} and {len(xv)}"
+
+        # Order the cached elements by unrotating
+        cache_k = [unrotate(t, s) for t, s in zip(self.cache_k, self.kv_seqlens)]
+        cache_v = [unrotate(t, s) for t, s in zip(self.cache_v, self.kv_seqlens)]
+
+        interleaved_k = interleave_list(cache_k, xk)
+        interleaved_v = interleave_list(cache_v, xv)
+
+        return torch.cat(interleaved_k, dim=0), torch.cat(interleaved_v, dim=0)
 
 
 @dataclass
@@ -53,6 +124,13 @@ class ModelArgs(Serializable):
     moe: Optional[MoeArgs] = None
 
 
+
+def repeat_kv(keys: torch.Tensor, values: torch.Tensor, repeats: int, dim: int):
+    keys = torch.repeat_interleave(keys, repeats, dim=dim)
+    values = torch.repeate_interleave(values, repeats, dim=dim)
+    return keys, values
+
+
 class Attention(nn.Module):
 
     def __init__(self, args: ModelArgs):
@@ -63,10 +141,12 @@ class Attention(nn.Module):
         self.head_dim = args.head_dim
         self.n_kv_heads = args.n_kv_heads
 
+        self.repeats = self.n_heads // self.n_kv_heads
 
         self.wq = nn.Linear(args.dim, args.n_heads * args.head_dim, bias=False)
         self.wk = nn.Linear(args.dim, args.n_kv_heads * args.head_dim, bias=False)
         self.wv = nn.Linear(args.dim, args.n_kv_heads * args.head_dim, bias=False)
+        self.wo = nn.Linear(args.n_heads * args.head_dim, args.dim, bias=False)
 
     def forward(self, x: torch.Tensor, freqs_cis: torch.Tensor, cache: Optional[CacheView]) -> torch.Tensor:
         seqlen_sum, _ = x.shape
@@ -78,7 +158,26 @@ class Attention(nn.Module):
         xq, xk = apply_rotary_emb(xq, xk, freqs_cis=freqs_cis)
 
         if cache is None:
+            key, val = xk, xv
+        elif cache.prefill:
+            key, val = cache.interleave_kv(xk, xv)
+            cache.update(xk, xv)
+        else:
+            cache.update(xk, xv)
+            key, val = cache.key, cache.value
+            key = key.view(seqlen_sum * cache.sliding_window, self.n_kv_heads, self.head_dim)
+            val = val.view(seqlen_sum * cache.sliding_window, self.n_kv_heads, self.head_dim)
 
+        # Repeat keys and values to match number of query heads
+        key, val = repeat_kv(key, val, self.repeats, dim=1)
+
+        # xformers requires (B=1, S, H, D)
+        xq, key, val = xq[None, ...], key[None, ...], val[None, ...]
+        output = memory_efficient_attention(
+            xq, key, val, None if cache is None else cache.mask
+        )
+
+        return self.wo(output.view(seqlen_sum, self.n_heads * self.head_dim))
 
 
 
@@ -89,6 +188,7 @@ class TransformerBlock(nn.Module):
         self.n_heads = args.n_heads
         self.dim = args.dim
         self.attention = Attention(args)
+
 
 
 
